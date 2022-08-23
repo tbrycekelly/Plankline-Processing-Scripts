37d79965aeb5a1414799c9f1fd973b3fe04fdcbc
Thomas Bryce Kelly <tbrycekelly@gmail.com>
2022-08-22 16:48:58 -0800

    Updates

--- R/Initial compilation.R
+++ R/Initial compilation.R
@@ -5,10 +5,11 @@ source('R/scripts.R')
 
 #### Project preparation and parameter setting
 
-base.dir = '/media/plankline/Data/Data/2022-07-22-17-18-24.796/'
+base.dir = '/media/plankline/Data/Data/2022-07-22_17-18-24.796/'
 out.dir = paste0(base.dir, 'R/')
 if (!dir.exists(out.dir)) { dir.create(out.dir)}
 
+frame.rate = 20.5
 
 #### Classification files
 # In this script, we will load data from each classification file, parse it into a summary data structure, and then save the results to an RDS file
@@ -59,8 +60,10 @@ for (i in 1:length(class.file)) {
 if (!dir.exists(paste0(out.dir, 'measurement/'))) { dir.create(paste0(out.dir, 'measurement/')) }
 
 for (i in 1:length(measure.file)) {
+  message('Processing file ', i, ' of ', length(measure.file), ' (', round(i*100/length(measure.file)), '%)')
+  
   ## Load file
-  summary = load.measurements(measure.file[i])
+  summary = load.measurements(measure.file[i], verbose = F)
   
   ## Save summary file
   saveRDS(summary, file = paste0(out.dir, 'measurement/', summary$image[1], ' measurement.rds'))
@@ -79,15 +82,18 @@ class.file = list.files(paste0(out.dir, 'classification/'),
                         pattern = '.rds',
                         full.names = T)
 
-part = data.frame()
+bin.width = 2 # second(s)
+
+if (!dir.exists(paste0(out.dir, 'bin/'))) { dir.create(paste0(out.dir, 'bin/')) }
+
+#part = data.frame()
 for (i in 1:length(class.file)) {
   message('Starting file ', i, ' of ', length(class.file), '... ')
   class = readRDS(class.file[i])
   
   ## Add timestamp
-  temp = strsplit(class$image, split = '-')
-  class$time = sapply(1:length(temp),
-                      function(x) {as.POSIXct(paste0(temp[[x]][3], '-', temp[[x]][4], '-', temp[[x]][5], ' ', temp[[x]][6], ':', temp[[x]][7], ':', temp[[x]][8]), tz = 'UTC')})
+  temp = strsplit(class$image[1], split = '-')[[1]]
+  class$time = as.POSIXct(paste0(temp[3], '-', temp[4], '-', temp[5], ' ', temp[6], ':', temp[7], ':', temp[8]), tz = 'UTC') + (class$frame - 1)/frame.rate
   
   ## Load measurements and add them if "they exist" and "number of entries matches"
   measure.file = gsub('classification', 'measurement', class.file[i])
@@ -97,13 +103,26 @@ for (i in 1:length(class.file)) {
     
     if (nrow(measure) == nrow(class)) {
       
-      ## Add necessary columns from measurement to calssificcation dataframe.
+      ## Add necessary columns from measurement to classification dataframe.
       for (n in colnames(measure)[!colnames(measure) %in% colnames(class)]) {
         class[[n]] = measure[[n]]
       }
       
+      
+      ## bin
+      class$time = as.POSIXct(round(as.numeric(class$time) / bin.width) * bin.width, origin = '1970-01-01')
+      bin = data.frame(time = unique(class$time))
+      
+      for (n in unique(class$class)) {
+        bin[[n]] = sapply(bin$time, function(x) {sum(class$time == x & class$class == n)})
+      }
+      bin$frames = sapply(bin$time, function(x) {diff(range(class$frame[class$time == x]))})
+      
+      ## output
+      saveRDS(bin, file = paste0(out.dir, 'bin/', class$image[1], ' bin.rds'))
+      
       ## Merge data into particle dataframe
-      part = rbind(part, class)
+      #part = rbind(part, class)
       
     } else {
       message('Uneven number of entries in classification and measurement files for a file! This is bad.')
@@ -114,17 +133,37 @@ for (i in 1:length(class.file)) {
   }
 }
 
-temp = strsplit(part$image, split = '-')
-part$time = sapply(1:length(temp),
-                   function(x) {as.POSIXct(paste0(temp[[x]][3], '-', temp[[x]][4], '-', temp[[x]][5], ' ', temp[[x]][6], ':', temp[[x]][7], ':', temp[[x]][8]), tz = 'UTC')})
 
+## load binned data
+bin.file = list.files(paste0(out.dir, 'bin/'),
+                        pattern = '.rds',
+                        full.names = T)
 
+bin = data.frame(time = 0)
 
-## Bin
-
-
-
-
+for (i in 1:length(bin.file)) {
+  message(i)
+  temp = readRDS(bin.file[i])
+  
+  bin.colnames = colnames(bin)
+  temp.colnames = colnames(temp)
+  
+  for (n in temp.colnames) {
+    if (!n %in% bin.colnames) {
+      bin[[n]] = NA
+    }
+  }
+  
+  for (n in colnames(bin)) {
+    if (!n %in% temp.colnames) {
+      temp[[n]] = NA
+    }
+  }
+  
+  ## Now merge!
+  bin = rbind(bin, temp)
+  
+}
 
 
 
