18e9aca4eccb3c6e3e863ab3c1dbc9410b02fbcb
Thomas Bryce Kelly <tbrycekelly@gmail.com>
2023-02-08 14:01:58 -0900

    Updated workflows

--- Workflows/Plankline A - Preprocesses Project.R
+++ Workflows/Plankline A - Preprocesses Project.R
@@ -3,37 +3,22 @@ library(PlanklinePS)
 
 #### Project preparation and parameter setting
 ## Setup
-root.dir = '/media/plankline/Data/Data/'
+base.dir = '/media/plankline/Data/Data/SNR10/'
 frame.rate = 20.5
-bin.width = 2 # second(s)
+bin.width = 1 # second(s)
 p.threshold = 0.5 # probability threshold
 
-dirs = list.dirs(root.dir, recursive = F, full.names = T)
-dirs = dirs[!grepl('test', dirs)]
-#dirs = dirs[grepl('Done', dirs)]
 
-
-for (base.dir in dirs) {
-  
-  message(Sys.time(), ': Starting to process ', base.dir)
-  
-  if (dir.exists(paste0(base.dir, '/segmentation')) & dir.exists(paste0(base.dir, '/classification'))) {
-  
-  out.dir = paste0(base.dir, '/R/')
-  if (!dir.exists(out.dir)) { dir.create(out.dir)}
-  
-  process.classifications(base.dir = base.dir,
-                          out.dir = out.dir)
-  
-  process.measurements(base.dir = base.dir,
-                       out.dir = out.dir)
-  
-  generate.bin(out.dir = out.dir,
-               p.threshold = p.threshold,
-               frame.rate = frame.rate,
-               bin.width = bin.width)
-  
-  } else {
-    message('Missing directories in ', base.dir,', skipping preprocessing.')
-  }
-}
+out.dir = paste0(base.dir, '/R/')
+if (!dir.exists(out.dir)) { dir.create(out.dir)}
+    
+process.classifications(base.dir = base.dir,
+                        out.dir = out.dir)
+    
+process.measurements(base.dir = base.dir,
+                     out.dir = out.dir)
+    
+generate.bin(out.dir = out.dir,
+             p.threshold = p.threshold,
+             frame.rate = frame.rate,
+             bin.width = bin.width)
--- Workflows/Plankline B - Compile Sensor and Bins.R
+++ Workflows/Plankline B - Compile Sensor and Bins.R
@@ -1,16 +1,22 @@
 library(PlanklinePS)
 library(TheSource)
 
-base.dir = '/media/plankline/Data/Data/2022-07-22_13-39-05.044/'
-#sensor = readRDS('/media/plankline/Data/Sensor/Transect 2022-07-22 022347.RDS')
-sensor = readRDS('/mnt/shuttle/Publish/Transect 2022-07-22 022347.RDS')
-
-sensor$Time = sensor$Time - 3600 * 8
-
+base.dir = '/media/plankline/Data/Data/SNR10/'
+transects = readRDS('/media/plankline/Data/Sensor/SKQJ2022_data/_rdata/transects.rds')
+transect.times = sapply(c(1:length(transects)), function(x) {mean(transects[[x]]$Time, na.rm = T)})
 
 #### Compile merged classification/sensor package dataset
 bin.files = list.files(paste0(base.dir, '/R/bin/'), pattern = '.rds', full.names = T)
 comp = data.frame(time = NA)
+
+bin = readRDS(bin.files[round(length(bin.files)/2)]) ## Load one bin file to determine datetime
+groups = sapply(strsplit(colnames(bin), split = '_'), function(x) {paste0(x[1], '_')})
+groups = unique(groups)
+
+## Determine what sesnor data goes with these files:
+transect.n = which.min((as.numeric(mean(bin$time, na.rm = T)) - transect.times)^2)
+sensor = transects[[transect.n]]
+sensor$Time = sensor$Time - 3600 * 8 ## Adj timezone
 
 for (i in 1:length(bin.files)) {
   message('Merging binned data and sensor data for file ', i, ' of ', length(bin.files), '...')
@@ -51,102 +57,122 @@ for (i in 1:length(bin.files)) {
 comp = comp[-1,]
 comp = comp[order(comp$time),]
 comp$Time = NULL
-comp[comp == Inf] = NA
+comp[comp == Inf] = 0
+comp[is.na(comp)] = 0
 comp = comp[, order(colnames(comp))]
 comp$time = as.POSIXct(comp$time, origin = '1970-01-01')
-saveRDS(comp, file = paste0(base.dir, '/R/compilation.rds'))
+saveRDS(comp, file = paste0(base.dir, '/R/Zooplankton Compilation.rds'))
 
-
-
-
-
-plot(x = comp$time,
-     y = comp$Depth,
-     col = make.pal(log10(comp$copepod_cyclopoid_oithona), min = 0, max = 1, pal = 'parula'),
-     pch = 20,
-     ylim = c(120,0),
-     yaxs = 'i')
-
-plot(x = comp$time,
-     y = comp$Depth,
-     col = make.pal(log10(group(comp, 'copepod_')), min = 0, max = 1.5, pal = 'parula'),
-     pch = 20,
-     ylim = c(120,0),
-     yaxs = 'i')
-
-
-plot(x = comp$time,
-     y = comp$Depth,
-     col = make.pal(comp$Temperature, pal = 'parula'),
-     pch = 20,
-     ylim = c(120,0),
-     yaxs = 'i')
-
-plot(x = comp$time,
-     y = comp$Depth,
-     col = make.pal(comp$SigmaTheta, pal = 'parula'),
-     pch = 20,
-     ylim = c(120,0),
-     yaxs = 'i')
-
-plot(x = comp$time,
-     y = comp$Depth,
-     col = make.pal(comp$frames, pal = 'parula'),
-     pch = 20,
-     ylim = c(120,0),
-     yaxs = 'i')
-
-
-plot(x = comp$time,
-     y = comp$Depth,
-     col = make.pal(group(comp, 'unknown'), pal = 'parula', min = 0, max = 10),
-     pch = 20,
-     ylim = c(120,0),
-     yaxs = 'i')
-
-plot(x = comp$time,
-     y = comp$Depth,
-     col = make.pal(group(comp, 'appendicularian_'), pal = 'parula', min = 0, max = 5),
-     pch = 20,
-     ylim = c(120,0),
-     yaxs = 'i')
-
-
-plot(x = comp$time,
-     y = comp$Depth,
-     col = make.pal(group(comp, 'protist_'), pal = 'parula', min = 0, max = 1),
-     pch = 20,
-     ylim = c(120,0),
-     yaxs = 'i')
-
-
-plot(x = comp$time,
-     y = comp$Depth,
-     col = make.pal(group(comp, 'detritus_'), pal = 'parula', min = 0, max = 100),
-     pch = 20,
-     ylim = c(120,0),
-     yaxs = 'i')
-
+{
+  pdf(paste0(base.dir, '/R/Sample Plots.pdf'))
+  par(mfrow = c(2,2))
+  
+  plot(x = comp$time,
+       y = comp$Depth,
+       col = make.pal(log10(comp$copepod_cyclopoid_oithona), min = 0, max = 1, pal = 'parula'),
+       pch = 20,
+       ylim = c(120,0),
+       yaxs = 'i')
+  
+  plot(x = comp$time,
+       y = comp$Depth,
+       col = make.pal(log10(group(comp, 'copepod_')), min = 0, max = 1.5, pal = 'parula'),
+       pch = 20,
+       ylim = c(120,0),
+       yaxs = 'i')
+  
+  plot(x = comp$time,
+       y = comp$Depth,
+       col = make.pal(group(comp, 'copepod_'), min = 0, max = 20, pal = 'parula'),
+       pch = 20,
+       ylim = c(120,0),
+       yaxs = 'i')
+  
+  
+  plot(x = comp$time,
+       y = comp$Depth,
+       col = make.pal(comp$Temperature, pal = 'parula'),
+       pch = 20,
+       ylim = c(120,0),
+       yaxs = 'i')
+  
+  plot(x = comp$time,
+       y = comp$Depth,
+       col = make.pal(comp$SigmaTheta, pal = 'parula'),
+       pch = 20,
+       ylim = c(120,0),
+       yaxs = 'i')
+  
+  plot(x = comp$time,
+       y = comp$Depth,
+       col = make.pal(comp$frames, pal = 'parula'),
+       pch = 20,
+       ylim = c(120,0),
+       yaxs = 'i')
+  
+  
+  plot(x = comp$time,
+       y = comp$Depth,
+       col = make.pal(group(comp, 'unknown'), pal = 'parula', min = 0, max = 10),
+       pch = 20,
+       ylim = c(120,0),
+       yaxs = 'i')
+  
+  plot(x = comp$time,
+       y = comp$Depth,
+       col = make.pal(group(comp, 'appendicularian_'), pal = 'parula', min = 0, max = 5),
+       pch = 20,
+       ylim = c(120,0),
+       yaxs = 'i')
+  
+  
+  plot(x = comp$time,
+       y = comp$Depth,
+       col = make.pal(group(comp, 'protist_'), pal = 'parula', min = 0, max = 1),
+       pch = 20,
+       ylim = c(120,0),
+       yaxs = 'i')
+  
+  
+  plot(x = comp$time,
+       y = comp$Depth,
+       col = make.pal(group(comp, 'detritus_'), pal = 'parula', min = 0, max = 100),
+       pch = 20,
+       ylim = c(120,0),
+       yaxs = 'i')
+  
+  
+  plot(log10(comp$Chl[comp$Depth < 20]),
+       log10(group(comp[comp$Depth < 20,], 'copepod_')),
+       pch = 20,
+       col = make.pal(comp$Depth[comp$Depth < 20],
+                      pal = 'ocean.deep', rev = T),
+       ylab = 'Copepods',
+       xlab = 'Chl',
+       xaxt = 'n',
+       yaxt = 'n')
+  
+  add.log.axis(1)
+  add.log.axis(2)
+  
+  dev.off()
+}
 
 ## Start preliminary plot
-groups = sapply(strsplit(colnames(comp), split = '_'), function(x) {paste0(x[1], '_')})
-groups = unique(groups)
-
 colnames(comp) = paste0(colnames(comp), '_')
-comp.reduced = data.frame(time = comp$time_)
+comp.reduced = data.frame(depth = comp$Depth_)
 
 for (g in groups) {
   comp.reduced[[g]] = group(comp, pattern = g, verbose = F)
 }
 
-
 section = build.section(x = comp.reduced$time_,
-                        y = comp.reduced$Depth_,
+                        y = comp.reduced$depth,
                         z = comp.reduced,
                         lat = comp.reduced$Latitude_,
                         lon = comp.reduced$Longitude_,
-                        y.factor = 0.5,
-                        neighborhood = 10,
+                        y.factor = 10,
+                        neighborhood = 50,
                         uncertainty = 2,
                         nx = 200,
                         ny = 200,
@@ -155,8 +181,8 @@ section = build.section(x = comp.reduced$time_,
                         field.names = colnames(comp.reduced))
 
 
-pdf(paste0(base.dir, '/R/Preliminary Sections.pdf'))
-par(mfrow = c(2,2))
+pdf(paste0(base.dir, '/R/Preliminary Zooplankton Sections Transect ', transect.n,'.pdf'))
+par(mfrow = c(2,1))
 for (i in 3:ncol(section$grid)) {
   
   plot.section(section,
@@ -169,23 +195,11 @@ for (i in 3:ncol(section$grid)) {
                xlab = '')
   
   axis.POSIXct(1, comp.reduced$time)
-  add.section.contour(section, field = 'Temperature_', col = 'red')
+  #add.section.contour(section, field = 'Temperature_', col = 'red')
   mtext(colnames(section$grid)[i], adj = 0)
 }
 
 dev.off()
 
+browseURL(paste0(base.dir, '/R/Preliminary Zooplankton Sections Transect ', transect.n,'.pdf'))
 
-
-
-pdf(paste0(base.dir, '/R/Section Map.pdf'))
-
-map = make.map.nga()
-add.map.points(map, lon = section$lon, lat = section$lat, col = 'dark red')
-add.map.contour(map,
-                bathy.pacific$Lon,
-                bathy.pacific$Lat,
-                z = bathy.pacific$Z,
-                levels = c(-100, -1000), col = 'grey')
-
-dev.off()
