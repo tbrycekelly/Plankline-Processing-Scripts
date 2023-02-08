aa3be07ee4f6e169e72273eeb674dce93efa8883
Thomas Bryce Kelly <tbrycekelly@gmail.com>
2023-02-08 14:13:06 -0900

    Add time

--- R/load.measurements.R
+++ R/load.measurements.R
@@ -10,7 +10,8 @@ load.measurements = function(file.path, verbose = T) {
   data = utils::read.csv(archive::archive_read(archive = file.path, file = f))
   
   ## setup final data structure and then populate with data
-  measurement.summary = data.frame(image = rep(NA, nrow(data)),
+  measurement.summary = data.frame(time = Sys.time(),
+                                   image = rep(NA, nrow(data)),
                                    frame = NA,
                                    roi = NA,
                                    area = NA,
@@ -23,6 +24,7 @@ load.measurements = function(file.path, verbose = T) {
                                    height = NA)
   
   if (verbose) { message('Identifying image and frame... ', appendLF = F)}
+  
   temp = strsplit(data$image[1], split = '/|_')[[1]]
   measurement.summary$image = paste(temp[length(temp)-4], temp[length(temp)-3], sep = '_')
   
@@ -41,6 +43,11 @@ load.measurements = function(file.path, verbose = T) {
   measurement.summary$y = data$y
   measurement.summary$mean = data$mean
   measurement.summary$height = data$height
+  
+  ## Get time
+  temp = strsplit(measurement.summary$image[1], split = '-')[[1]]
+  measurement.summary$time = as.POSIXct(paste0(temp[3], '-', temp[4], '-', temp[5], ' ', temp[6], ':', temp[7], ':', temp[8]), tz = 'UTC') + (measurement.summary$frame - 1) / frame.rate
+  
   if (verbose) { message('Done.') }
   
   ## Return 
