854c2541f8887b4ce6ed705f4447d405158ded7d
Thomas Bryce Kelly <tbrycekelly@gmail.com>
2023-02-22 15:35:34 -0900

    Update load.suna.files.R

--- R/load.suna.files.R
+++ R/load.suna.files.R
@@ -18,16 +18,19 @@ load.suna.files = function(files, skip = 21, min.size = 1024, verbose = T) {
   ## Load in first file.
   message(' Attempting to load file 1... ', appendLF = F)
   dat = data.table::fread(files[1], skip = skip, fill = T)
-  dat = data.frame(Time = conv.time.unix(make.time(year = substr(dat$V2, 1, 4)) + 86400 * as.numeric(substr(dat$V2, 5, 7)) + 3600 * dat$V3),
+  dat = data.frame(Time = make.time(year = substr(dat$V2, 1, 4)) + 86400 * as.numeric(substr(dat$V2, 5, 7)) + 3600 * dat$V3,
                    Nitrate = dat$V4)
+  dat$Time = as.numeric(dat$Time)
   message('Done.')
   
   ## Read in rest of data
   for (i in 2:length(files)) {
     message(' Attempting to load file ', i, '... ', appendLF = F)
     temp = data.table::fread(files[i], skip = skip, fill = T, verbose = F, showProgress = F)
-    temp = data.frame(Time = conv.time.unix(make.time(year = substr(temp$V2, 1, 4)) + 86400 * as.numeric(substr(temp$V2, 5, 7)) + 3600 * temp$V3),
+    temp = data.frame(Time = make.time(year = substr(temp$V2, 1, 4)) + 86400 * as.numeric(substr(temp$V2, 5, 7)) + 3600 * temp$V3,
                      Nitrate = temp$V4)
+    temp$Time = as.numeric(temp$Time)
+    
     if (ncol(temp) != ncol(dat)) {
       message('Invalid number of columns (', ncol(temp), ' vs ', ncol(dat), '), skipping.')
     } else {
